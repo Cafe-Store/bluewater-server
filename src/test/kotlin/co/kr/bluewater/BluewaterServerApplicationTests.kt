1163a026229b7322979e0009fdefeefc52cfc473
jito-d <jito.d@kakaopaycorp.com>
2021-08-02 22:49:35 +0900

    project setting

---
+++ src/test/kotlin/co/kr/bluewater/BluewaterServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.kr.bluewater
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BluewaterServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
