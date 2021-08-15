52a51d5d9b37462648d2239f520429c042db670c
jito-d <jito.d@kakaopaycorp.com>
2021-08-15 23:31:54 +0900

    shop 정보에 이미지 주소 추가하기

--- src/main/kotlin/co/kr/bluewater/domain/shop/Shop.kt
+++ src/main/kotlin/co/kr/bluewater/domain/shop/Shop.kt
@@ -9,5 +9,16 @@ data class Shop(
     val shopId: String,
     val name: String,
     val rank: Rank,
-    val products: MutableSet<String>
-)
+    val products: MutableSet<String>,
+    val imageUrl: String
+) {
+
+    constructor(shopId: String, name: String, rank: Rank, products: MutableSet<String>): this(
+        shopId = shopId,
+        name = name,
+        rank = rank,
+        products = products,
+        imageUrl = "https://cdn.stocksnap.io/img-thumbs/960w/food-fruits_N7YQYVFWOU.jpg"
+    )
+
+}
