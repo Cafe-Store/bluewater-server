d328414812c751a1b00407122cda48b4002f0b80
jito-d <jito.d@kakaopaycorp.com>
2021-10-21 23:30:37 +0900

    Shop 에 영업시간, 가게위치 등 추가

---
+++ src/main/kotlin/co/kr/bluewater/domain/Address.kt
@@ -0,0 +1,7 @@
+package co.kr.bluewater.domain
+
+class Address(
+    val postcode: String,
+    val street: String,
+    val detail: String
+)
--- src/main/kotlin/co/kr/bluewater/domain/shop/Shop.kt
+++ src/main/kotlin/co/kr/bluewater/domain/shop/Shop.kt
@@ -1,8 +1,7 @@
 package co.kr.bluewater.domain.shop
 
+import co.kr.bluewater.domain.*
 import co.kr.bluewater.domain.category.Category
-import co.kr.bluewater.domain.Photo
-import co.kr.bluewater.domain.Rank
 import java.util.*
 
 /**
@@ -11,19 +10,34 @@ import java.util.*
 class Shop(
     val shopId: String,
     val name: String,
+    val address: Address,
+    val tel: TelephoneNumber,
+    val businessNumber: BusinessNumber,
     val rank: Rank,
     val products: MutableSet<String>,
     val photos: MutableSet<ShopPhoto>,
-    val owner: Owner? = null,
-    val categories: MutableSet<Category>? = null
+    val owner: Owner,
+    val officeHours: String,
+    val notice: String,
+    val categories: MutableSet<Category>
 ) {
 
     constructor(name: String, photo: Photo, categories: MutableSet<Category>):this(
-        shopId = UUID.randomUUID().toString().substring(0,15),
+        shopId = UUID.randomUUID().toString().replace("-", ""),
         name = name,
+        address = Address(
+            postcode = "12345",
+            street = "경기도 성남시 분당구 판교역로 152",
+            detail = "알파돔타워"
+        ),
+        officeHours = "월-목: 10:00 ~ 23:00\n금-일: 10:30 ~ 23:00",
+        notice = "매장의 공지사항은 이렇습니다.\n어쩌구 저쩌구 이러쿵 저러쿵 입니다.\n그렇지만 맛은 좋습니다.\n하지만 신발가게라서 드시면 안됩니다.",
+        owner = Owner("김사장"),
+        tel = TelephoneNumber("031-1234-1234"),
+        businessNumber = BusinessNumber("111-80-12323"),
         rank = Rank(),
         products = mutableSetOf(),
-        photos = mutableSetOf(ShopPhoto(main = true, photo = photo)),
+        photos = mutableSetOf(ShopPhoto(main = true, photo = photo), ShopPhoto(photo = photo), ShopPhoto(photo = photo)),
         categories = categories
     )
 }
