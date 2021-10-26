d96df68f9bf9ed120d9f90931c385a00fba7ae63
jito-d <jito.d@kakaopaycorp.com>
2021-10-26 16:49:08 +0900

    Bugfix / BaseDocument 를 사용하면서 collection 들에 주입되는 데이터 필드명 수정

--- src/main/kotlin/co/kr/bluewater/infra/CategoryMongoRepository.kt
+++ src/main/kotlin/co/kr/bluewater/infra/CategoryMongoRepository.kt
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface CategoryMongoRepository:MongoRepository<CategoryDocument, String> {
-    @Query("{'code' : ?0}")
+    @Query("{'data.code' : ?0}")
     fun findByCode(code: String): CategoryDocument
 }
 
